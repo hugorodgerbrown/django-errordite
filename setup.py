37c4e85e2a9c381258f7eee3c9bcecee2b83ae9c
hugo@home <hugo@rodger-brown.com>
2013-03-03 10:08:56 +0000

    Fix for circular import issue, and bump in version to 0.3.1

--- setup.py
+++ setup.py
@@ -4,22 +4,33 @@ Setup file for django-errordite.
 import os
 from os.path import join, dirname, normpath, abspath
 from setuptools import setup
-import django_errordite
 
 # allow setup.py to be run from any path
 os.chdir(normpath(join(abspath(__file__), os.pardir)))
 
+# this is a duplicate of django_errordite.__init__.py - which we can't
+# use because of a circular dependency issue (relies on errordite, which
+# may not be installed).
+__title__ = 'django_errordite'
+__version__ = '0.3.1'
+__author__ = 'Hugo Rodger-Brown'
+__license__ = 'Simplified BSD License'
+__copyright__ = 'Copyright 2013 Hugo Rodger-Brown'
+__description__ = 'Errordite exception logging for Django.'
+__long_desc__ = open(join(dirname(__file__), 'README.rst')).read()
+__license__ = open(join(dirname(__file__), 'LICENCE.md')).read()
+
 setup(
-    name=django_errordite.__title__,
-    version=django_errordite.__version__,
+    name=__title__,
+    version=__version__,
     packages=['django_errordite'],
     install_requires=['errordite>=0.3'],
     include_package_data=True,
-    license=open(join(dirname(__file__), 'LICENCE.md')).read(),
-    description=django_errordite.__description__,
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    license=__license__,
+    description=__description__,
+    long_description=__long_desc__,
     url='https://github.com/hugorodgerbrown/python-errordite',
-    author=django_errordite.__author__,
+    author=__author__,
     author_email='hugo@rodger-brown.com',
     classifiers=[
         'Environment :: Web Environment',
