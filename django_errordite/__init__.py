2069020f384a43721368fe3497350ad26fc1540d
Hugo Rodger-Brown <hugo@rodger-brown.com>
2013-04-30 12:23:20 +0100

    Bumps version to 0.7

--- django_errordite/__init__.py
+++ django_errordite/__init__.py
@@ -2,7 +2,7 @@
 Specialised framework-specific Errordite log handlers.
 """
 __title__ = 'django-errordite'
-__version__ = '0.6'
+__version__ = '0.7'
 __author__ = 'Hugo Rodger-Brown'
 __license__ = 'Simplified BSD License'
 __copyright__ = 'Copyright 2013 Hugo Rodger-Brown'
